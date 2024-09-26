73bf88ed1e2eee155b2b3cce59b4ecaa9f5dcdd2
Cristian-Miguel <cristian_enano_99@hotmail.com>
2024-09-25 19:57:46 -0600

    first commit

---
+++ src/test/java/com/news/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.news.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
